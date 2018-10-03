d9c3be6c88950b78ac3c3278cc3dd08eda69f843
Tomer Keren <tomer.keren.dev@gmail.com>
2018-10-03 18:05:30 +0300

    :sparkles:Extract python docstring from module

--- src/py_comment_extractor.py
+++ src/py_comment_extractor.py
@@ -1,8 +1,14 @@
 from pathlib import Path
 import importlib.util
-def import_module(module_path:str):
+from types import ModuleType
+
+
+def import_module(module_path: str) -> ModuleType:
     module_path = Path(module_path)
     spec = importlib.util.spec_from_file_location(module_path.name, module_path)
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
     return module
+
+def module_docstring(module_path:str) -> str:
+    return import_module(module_path).__doc__
--- tests/test_py_comment_extractor.py
+++ tests/test_py_comment_extractor.py
@@ -1,7 +1,7 @@
 """Test file for the python comment extractor"""
 import os
 
-from src.py_comment_extractor import import_module
+from src.py_comment_extractor import import_module, module_docstring
 
 FILE_PATH = os.path.abspath(__file__)
 
@@ -9,3 +9,7 @@ FILE_PATH = os.path.abspath(__file__)
 def test_import_module():
     this_module = import_module(FILE_PATH)
     assert 'test_import_module' in dir(this_module)
+
+def test_module_docstring():
+    assert __doc__ == module_docstring(FILE_PATH)
+    
