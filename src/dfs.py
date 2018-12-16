0caa9126acd9edcb49d2d035a4e92b2ccdd30eb8
Tomer Keren <tomer.keren.dev@gmail.com>
2018-12-16 20:37:29 +0200

    Fix indent by not including root in dfs results

--- src/dfs.py
+++ src/dfs.py
@@ -1,10 +1,10 @@
 """Generic and pythonic implementation of the Depth First Search traversal algorithm"""
 from pathlib import Path
-from typing import (Any, Callable, Generator,  Hashable, Iterable,
-                    List, NamedTuple, Set, TypeVar, Generic, MutableSequence)
+from typing import (Callable, Generator, Iterable, List, MutableSequence,
+                    NamedTuple, Optional, Set, TypeVar)
 
 NodeType = Path
-StackType = TypeVar('StackType', bound=Any)
+StackType = TypeVar('StackType')
 
 
 class DfsVisit(NamedTuple):
@@ -12,7 +12,7 @@ class DfsVisit(NamedTuple):
     depth: int
 
 
-def iter_top(stack: MutableSequence[StackType])->Iterable[StackType]:
+def iter_top(stack: MutableSequence[StackType]) -> Iterable[StackType]:
     """Iterate a stack as long as it is full, yielding the top every time"""
     while stack:
         yield stack.pop()
@@ -20,13 +20,13 @@ def iter_top(stack: MutableSequence[StackType])->Iterable[StackType]:
 
 def dfs(root: NodeType, neighbors_func: Callable[[NodeType], Iterable[NodeType]],
         *,  # Every argument after this has to be a keyword argument
-        predicate: Callable[[NodeType], bool]=None,
-        max_depth: int=None)->Generator[DfsVisit, None, None]:
-    """Iterate starting from root, going depth first
+        predicate: Callable[[NodeType], bool] = None, max_depth: Optional[int] = None) -> Generator[DfsVisit, None, None]:
+    """
+    Iterate starting from root, going depth first
     `neighbors_func`: a function that returns an iterable of the nodes neighbors
     returns: A tuple of (item, depth), in order of dfs traversal
         """
-    stack: List[DfsVisit] = [DfsVisit(root, 0)]
+    stack: List[DfsVisit] = [DfsVisit(root, -1)]
     visited_nodes: Set[NodeType] = set()
     for item, depth in ((item, depth) for item, depth in iter_top(stack)
                         if item not in visited_nodes
@@ -34,19 +34,9 @@ def dfs(root: NodeType, neighbors_func: Callable[[NodeType], Iterable[NodeType]]
                         and (max_depth is None or depth <= max_depth)):
         visited_nodes.add(item)
         stack.extend(DfsVisit(item, depth+1) for item in neighbors_func(item))
-        yield DfsVisit(item, depth)
+        if item != root:
+            yield DfsVisit(item, depth)
 
 
 def safe_iterdir(path: Path):
     return path.iterdir() if path.is_dir() else tuple()
-
-
-def main():
-    root = Path(Path.home()/'Desktop')
-    path: Path
-    return ('\n'.join(str(path.resolve())
-                      for path, depth in dfs(root, safe_iterdir) if path.is_dir()))
-
-
-if __name__ == '__main__':
-    print(main())
